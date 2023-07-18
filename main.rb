48c606e40795b36783f6c015f31f1824272382f2
Shihab Uddin Khan <aimshihabuddinkhan@gmail.com>
2023-07-18 18:48:06 +0000

    Add string and string operations

--- main.rb
+++ main.rb
@@ -1,20 +1,65 @@
-puts "hello world"
-load 'side.rb'
-p "hello world"
+# puts "hello world"
+# load 'side.rb'
+# p "hello world"
 
-# hellsoafj --comment
+# # hellsoafj --comment
 
-#variables
-name="rafio"
-puts name
+# #variables
+# name="rafio"
+# puts name
 
-num= 122
-bool= true
+# num= 122
+# bool= true
 
-puts name, num,bool
+# puts name, num,bool
 
-puts "Hello #{name}, your number is #{num} which is #{bool}"
+# puts "Hello #{name}, your number is #{num} which is #{bool}"
 
-print "give me all your money"
-money= get.chomp
-puts "what only this?? #{money}"
+# print "give me all your money"
+# money= gets.chomp.to_i
+# puts "what only this?? #{money}"
+
+# print "What is your name?"
+# name= gets.chomp
+# puts "My name is #{name}"
+
+# puts "This is
+# multiline 
+# string"
+
+# str = 'happy'
+# p str.size
+# p str.length
+# p str.upcase
+# p str.downcase
+# p str.reverse
+# p str.upcase.reverse
+
+# p str.include? "happy"
+
+# p "hello" "   how are you?"
+# p "hello"+" how are you?"
+
+# p "hello".concat(" how are you?")
+
+# str = 'belly'
+
+# p str
+# str.freeze
+# str = str << 'dance'
+# p str
+
+# p 'abc' == 'abc'
+# p 123==72
+# p 'abc'.eql? 'abc'
+
+# str = "Hello I am Rafio Learning about Ruby"
+# puts str
+# puts str["Hello"]
+# puts str[0]
+# puts str[0,11]
+# puts str[0,str.length]
+# puts str[-4]
+# puts str.length
+# puts str[0,15]    # it will go until index 14 
+# puts str[0..15]    # it will go until index 15
