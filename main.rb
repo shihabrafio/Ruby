15d69ef22f99f958f14cd5027419dd41743e9b96
Shihab Uddin Khan <aimshihabuddinkhan@gmail.com>
2023-07-18 14:47:06 +0000

    Add variable and print

---
+++ main.rb
@@ -0,0 +1,20 @@
+puts "hello world"
+load 'side.rb'
+p "hello world"
+
+# hellsoafj --comment
+
+#variables
+name="rafio"
+puts name
+
+num= 122
+bool= true
+
+puts name, num,bool
+
+puts "Hello #{name}, your number is #{num} which is #{bool}"
+
+print "give me all your money"
+money= get.chomp
+puts "what only this?? #{money}"
